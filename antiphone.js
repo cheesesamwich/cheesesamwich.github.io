c556b5b34b0e113b63ce47e424555084061fda05
Sam <149597648+cheesesamwich@users.noreply.github.com>
2023-11-28 17:15:26 +0000

    hopefully fixed anti phone

--- antiphone.js
+++ antiphone.js
@@ -1,3 +1,8 @@
-if (!/(Win|Mac|Linux)/i.test(navigator.platform)) {
+console.log(navigator.platform); // Outputs the platform to the console for debugging.
+
+if (!/(Win|Mac|Linux)/i.test(navigator.platform) && !window.location.href.includes('nophone.html')) {
+    console.log('Redirecting to nophone.html');
     window.location.href = 'nophone.html';
+} else {
+    console.log('No redirection necessary.');
 }
