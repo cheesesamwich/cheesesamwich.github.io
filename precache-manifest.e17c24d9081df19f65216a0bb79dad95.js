75dd24ee8e3f8de307c52938d5e345508c59a7a9
Sam <cheesesamwich@proton.me>
2024-02-14 18:03:05 +0000

    Updates

---
+++ precache-manifest.e17c24d9081df19f65216a0bb79dad95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783461d9dcbbc6113b824a9c0c03b2f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "151160c62392ed463b89",
+    "url": "/static/css/main.399df930.chunk.css"
+  },
+  {
+    "revision": "b6d5b3eb25ab9694b855",
+    "url": "/static/js/2.1d8f9e5d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.1d8f9e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151160c62392ed463b89",
+    "url": "/static/js/main.bd3c3309.chunk.js"
+  },
+  {
+    "revision": "56a4317fa1601260a350",
+    "url": "/static/js/runtime-main.fd1e8490.js"
+  }
+]);
