622ae66776455eae6a9e6590a103dcc8e93d0f40
Sam <149597648+cheesesamwich@users.noreply.github.com>
2024-03-16 22:22:57 +0000

    Create HitSounds.cs

---
+++ scripts/HitSounds.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HitSounds : MonoBehaviour
+{
+    public List<SoundGroup> soundGroups;
+
+    [System.Serializable]
+    public class SoundGroup 
+    {
+        public List<AudioClip> sounds;
+        public List<Material> materials;
+    }
+
+    public void SoundTriggered(GameObject hitter, GameObject hit) 
+    {
+        Material material = hit.GetComponent<MeshRenderer>().sharedMaterial;
+        SoundGroup group = GetGroupForMaterial(material);
+
+        if (group == null) return;
+        AudioClip sound = group.sounds[Random.Range(0, group.sounds.Count)];
+        hitter.GetComponent<AudioSource>().PlayOneShot(sound);
+    }
+
+    public SoundGroup GetGroupForMaterial(Material targetMaterial)
+    {
+        foreach (SoundGroup soundGroup in soundGroups)
+        {
+            if (soundGroup.materials.Contains(targetMaterial))
+            {
+                return soundGroup;
+            }
+        }
+        return null;
+    }
+}
