6bbf20ba32f6c6715302db3b5766e9a0b4ed0943
= <cheesesamwich@proton.me>
2024-03-19 15:44:07 +0000

    Add handling for site ids

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './style.css';
 const scripts = getScripts();
 const tuts = getTuts();
 
-function getElementFromName(name)
+function getElementFromID(id)
 {
-  if(name == null | undefined) { return null; }
-  return scripts.concat(tuts).find(video => video.name.toLowerCase() === name.toLowerCase());
+  if(id == (undefined || null)) { return null; }
+  return scripts.concat(tuts).find(video => video.id === id && video.id != null | undefined);
 }
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -34,18 +34,16 @@ function App() {
     }
   };
 
-  let gotInfo = getElementFromName(infoPage);
+  let gotInfo = getElementFromID(infoPage);
   let isInfoGood = gotInfo !== undefined | null;
 
-  let safeInfo = isInfoGood ? gotInfo : null;
-
   let safeDescription, safeName;
-  if (safeInfo !== null && safeInfo !== undefined) {
-    safeDescription = safeInfo.safeDescription;
-    safeName = safeInfo.safeName;
+  if (gotInfo != null | undefined) {
+    safeDescription = gotInfo.description;
+    safeName = gotInfo.name;
   } else {
-    safeDescription = "";
-    safeName = "";
+    safeDescription = "error";
+    safeName = "error";
   }
 
   return (
@@ -86,7 +84,7 @@ function App() {
             </div>
         </>
       }
-      {infoPage && <div id='scriptInfo'><ScriptInfo scriptName={infoPage}></ScriptInfo></div>}  
+      {infoPage && <div id='scriptInfo'><ScriptInfo data={gotInfo}></ScriptInfo></div>}  
       <a className='rounded-link discordButton buttonAnim' href="https://discord.gg/SvJMAnUgM3">
         <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 24 24" style={{scale:"1.3"}}>
           <path fill="currentColor" d="M19.73 4.87a18.2 18.2 0 0 0-4.6-1.44c-.21.4-.4.8-.58 1.21-1.69-.25-3.4-.25-5.1 0-.18-.41-.37-.82-.59-1.2-1.6.27-3.14.75-4.6 1.43A19.04 19.04 0 0 0 .96 17.7a18.43 18.43 0 0 0 5.63 2.87c.46-.62.86-1.28 1.2-1.98-.65-.25-1.29-.55-1.9-.92.17-.12.32-.24.47-.37 3.58 1.7 7.7 1.7 11.28 0l.46.37c-.6.36-1.25.67-1.9.92.35.7.75 1.35 1.2 1.98 2.03-.63 3.94-1.6 5.64-2.87.47-4.87-.78-9.09-3.3-12.83ZM8.3 15.12c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.89 2.27-2 2.27Zm7.4 0c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.88 2.27-2 2.27Z" className=""></path>
@@ -97,14 +95,12 @@ function App() {
 }
 
 function ScriptInfo(props) {
-  let infoElement;
-  infoElement = getElementFromName(props.scriptName);
   
-  if (infoElement) {
+  if (props.data) {
     return (
       <div>
         <div>
-          {infoElement.tutorial}
+          {props.data.tutorial}
         </div>
       </div>
     );
@@ -130,6 +126,7 @@ function ScriptTiles({ searchTerm, videos, download }) {
         tutorial={element.tutorial}
         videos={videos}
         download={download}
+        scriptID={element.id}
       />
     );
   });
@@ -179,7 +176,7 @@ function ScriptTile(props) {
             </svg>
           </button>
         }
-        <a className='rounded-link buttonAnim' href={`?info=${props.scriptName}`}>
+        <a className='rounded-link buttonAnim' href={`?info=${props.scriptID}`}>
           <svg data-slot="icon" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" id='linkIcon'>
               <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"></path>
           </svg>  
